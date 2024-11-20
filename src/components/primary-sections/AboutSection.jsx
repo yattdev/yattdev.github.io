9b4f61b5e578a6ee17b3a37b4877cddbdb538d07
Alassane Yattara <alassane.yattara@savoirfairelinux.com>
2024-11-20 15:45:58 +0100

    Update content of section about me from AboutSection

--- src/components/primary-sections/AboutSection.jsx
+++ src/components/primary-sections/AboutSection.jsx
@@ -8,35 +8,34 @@ const AboutSection = () => {
           <h2 className="heading">About Me</h2>
           <div className="content">
             <p className="text-start">
-              Alassane Yattara is an experienced software engineer and
-              passionate Starter (Creator). Alassane currently holds a master's
-              degree in computer science from Institute Polytechnic Private of
-              Casablanca, Morocco. After his bachelor's degree in Mathematics,
-              Physics and Chemistry in &nbsp;
+              <h5>Yo ! 🙂</h5> I'm Alassane Yattara, or simply <strong>Yatt</strong>.
+              I’m an experienced <a href="https://www.theforage.com/blog/careers/full-stack-developer-vs-software-engineer">Full Stack Software Engineer and Consultant Developer</a>
+               &nbsp; with a proven track record of designing, implementing,
+              and testing complex web applications, RESTful APIs,
+              AI-powered generative solutions, and games. I hold a Master’s
+              degree in Computer Science from the Institut Polytechnique Privé
+              of Casablanca, Morocco, building on a foundation of Mathematics,
+              Physics, and Chemistry from my bachelor’s studies in &nbsp;
               <a href="https://fr.wikipedia.org/wiki/Bamako">Bamako, Mali</a>.
               <br />
               <br />
-              Committed starter, Detail-oriented engineer, Manager, Alassane has
-              designed, implemented and tested complex web applications, &nbsp;
-              <a href="https://fr.wikipedia.org/wiki/Representational_state_transfer">
-                Restfull API
+              
+              <a href="https://www.linkedin.com/pulse/20140615184933-46729331-being-flexible-and-versatile">
+                  <h5>💪💥 Versatile</h5>
               </a>
-              , Assistant personnel intelligent (Virtual assistant) in &nbsp;
-              <a href="https://towardsdatascience.com/natural-language-processing-nlp-for-machine-learning-d44498845d5b">
-                NPL
-              </a>
-              &nbsp; and experienced Machine Learning (ML), using a variety of
-              programming languages and technologies. Alassane also learns
-              quickly with strong problem-solving, leadership, management and
-              communication skills, comfortable working in a team or
-              individually.
+              I thrive on tackling challenges with 
+              innovative solutions. My strong problem-solving abilities,
+              coupled with effective management and communication skills,
+              make me equally comfortable working collaboratively in a team or
+              independently, whether on-site or remotely.
               <br />
               <br />
-              In his spare time, Alassane absorbs books, shares this knowledge,
-              contributes to the community in daily actions and also develops
-              &nbsp;
-              <a href="https://github.com/yattdev/">Open source</a> &nbsp;
-              software.
+              <h5>💓</h5>
+              Beyond my professional endeavors, I am passionate about continuous
+              learning, sharing knowledge, and contributing to the community
+              through meaningful actions. In my free time, I enjoy reading,
+              developing startup or open-source software 😎, and exploring new ways to give 
+              back to the tech ecosystem.
             </p>
           </div>
         </div>
